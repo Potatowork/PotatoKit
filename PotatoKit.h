cc00acc44c1006f40e290e430325bc2029cc862c
EasyGet <371814174@qq.com>
2018-08-15 23:08:30 +0800

    Infor files

---
+++ PotatoKit.h
@@ -0,0 +1,19 @@
+//
+//  PotatoKit.h
+//  PotatoKit
+//
+//  Created by Richie on 2018/8/15.
+//  Copyright © 2018 person. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PotatoKit.
+FOUNDATION_EXPORT double PotatoKitVersionNumber;
+
+//! Project version string for PotatoKit.
+FOUNDATION_EXPORT const unsigned char PotatoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PotatoKit/PublicHeader.h>
+
+
